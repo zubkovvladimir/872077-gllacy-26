b58e9786955f81b72a1a763949903af9947a24cd
zubkovvladimir <njoy70@mail.ru>
2019-10-31 22:06:49 +0500

    типа финал

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var feedback = document.querySelector(".address__btn");
+var popup = document.querySelector(".modal");
+var popupwrap = document.querySelector(".modal-wrap");
+var popupclose = document.querySelector(".modal__btn-close");
+var popupform = document.querySelector(".modal__form");
+var username = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
+var storageUsername = localStorage.getItem("username");
+var storageEmail = localStorage.getItem("email");
+
+feedback.addEventListener("click",function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    popupwrap.classList.add("modal-wrap__show");
+    username.focus();
+    if(storageUsername && storageEmail){
+        username.value = storageUsername;
+        email.value = storageEmail;
+    }
+});
+
+popupclose.addEventListener("click",function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popupwrap.classList.remove("modal-wrap__show");
+    popup.classList.remove("modal-error");
+});
+
+popupwrap.addEventListener("click",function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popupwrap.classList.remove("modal-wrap__show");
+});
+
+popupform.addEventListener("submit",function (evt) {
+    if(!username.value || !email.value || !text.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+    } else {
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("email", email.value);
+    }
+});
